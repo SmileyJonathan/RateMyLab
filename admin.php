ec333edc0bcf4fbd2b988ae1203abf924d1806ab
JonathanSmiley <jsmiley0614@gmail.com>
2022-11-30 17:28:29 -0500

    Logins
    Login in function for professor and admin
    
    professor.html and admin.html had to be converted to php files in order for the php run

---
+++ admin.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="admin.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@300400,500,600,700,800,900&display=swap"
+      rel="stylesheet"
+    />
+    <script
+      src="https://kit.fontawesome.com/6df089f401.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <form action="alogin.php" method="post">
+        <div class="content">
+        <div class="img-container">
+            <img src="https://raw.githubusercontent.com/CSC4350-TR/RateMyLab/main/RateMyLabassets/Logo.png" alt="Georgia State University" />
+        </div>
+        <h1>Admin Login</h1>
+        <p>Please enter your credentials</p>
+
+        <?php if (isset($_GET['error'])) { ?>
+        <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
+
+        <label>Username</label>
+
+        <input id="username-input" name="user" type="text">
+        </input>
+
+        <label>Password</label>
+
+        <input id="password-input" name="password" type="password">
+        </input>
+
+        <button type=submit id=login>Login</a>
+        </div>
+    </form>
+  </body>
+</html>
---
+++ plogin.php
@@ -0,0 +1,93 @@
+<?php 
+
+session_start(); 
+
+$host="127.0.0.1";
+$port=3306;
+$socket="";
+$user="root";
+$password="";
+$dbname="ratemylab";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+if (isset($_POST['user']) && isset($_POST['password'])) {
+
+    function validate($data){
+
+       $data = trim($data);
+
+       $data = stripslashes($data);
+
+       $data = htmlspecialchars($data);
+
+       return $data;
+
+    }
+
+    $uname = validate($_POST['user']);
+
+    $pass = validate($_POST['password']);
+
+    if (empty($uname)) {
+
+        header("Location: professor.php?error=User Name is required");
+
+        exit();
+
+    }else if(empty($pass)){
+
+        header("Location: professor.php?error=Password is required");
+
+        exit();
+
+    }else{
+
+        $sql = "SELECT * FROM logins WHERE username ='$uname' AND password='$pass'";
+
+        $result = mysqli_query($con, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+
+            $row = mysqli_fetch_assoc($result);
+
+            if ($row['username'] === $uname && $row['password'] === $pass) {
+
+                echo "Logged in!";
+
+                $_SESSION['user_name'] = $row['user_name'];
+
+                $_SESSION['name'] = $row['name'];
+
+                $_SESSION['id'] = $row['id'];
+
+                header("Location: rateMyLab.html");
+
+                exit();
+
+            }else{
+
+                header("Location: professor.php?error=Incorect User name or password");
+
+                exit();
+
+            }
+
+        }else{
+
+            header("Location: professor.php?error=Incorect User name or password");
+
+            exit();
+
+        }
+
+    }
+
+}else{
+
+    header("Location: profesor.php");
+
+    exit();
+
+}
