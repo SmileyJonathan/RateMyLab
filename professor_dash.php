1fa9b5c62a7990996e4fef16d454f5205f6910c7
JonathanSmiley <jsmiley0614@gmail.com>
2022-12-30 10:24:36 -0500

    Changing page titles

--- professor_dash.php
+++ professor_dash.php
@@ -21,7 +21,7 @@ $name = $_SESSION['name'];
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Professor Dashboard</title>
     <link rel="stylesheet" href="professor_dash.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
